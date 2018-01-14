ea80b9b53b11daba226a149b8b7f3b270fb2e33d
Felipe de Alencar Pinheiro <felipe.pinheiro.90@hotmail.com>
2018-01-14 13:08:27 -0200

    Adds the endpoint to get the most completed flows

--- routes/report.js
+++ routes/report.js
@@ -1,11 +1,22 @@
 const router = require('express').Router();
 const service = require('../services/report');
 
+router.get('/most-completed-flows', (req, res) => {
+  service
+    .getMostCompletedFlows()
+    .then((flows) => {
+      res.status(200).json(flows);
+    })
+    .catch(() => {
+      res.sendStatus(500);
+    });
+});
+
 router.get('/most-used-flows', (req, res) => {
   service
     .getMostUsedFlows()
-    .then((mostUsedFlows) => {
-      res.status(200).json(mostUsedFlows);
+    .then((flows) => {
+      res.status(200).json(flows);
     })
     .catch(() => {
       res.sendStatus(500);
--- services/report.js
+++ services/report.js
@@ -1,22 +1,38 @@
 const Activity = require('../models/activity');
 
-const mostUsedFlowsAggregationPipeline = [
-  { $match: { type: 'flow', status: 'start' } },
-  { $sortByCount: '$flow' },
-  {
-    $lookup: {
-      from: 'flows',
-      localField: '_id',
-      foreignField: '_id',
-      as: 'flows',
+function createFlowAggregationPipeline(status) {
+  return [
+    { $match: { type: 'flow', status } },
+    { $sortByCount: '$flow' },
+    {
+      $lookup: {
+        from: 'flows',
+        localField: '_id',
+        foreignField: '_id',
+        as: 'flows',
+      },
     },
-  },
-  { $project: { _id: 0, flow: { $arrayElemAt: ['$flows', 0] }, count: 1 } },
-  { $project: { flow: { id: '$flow._id', title: 1 }, count: 1 } },
-];
+    { $project: { _id: 0, flow: { $arrayElemAt: ['$flows', 0] }, count: 1 } },
+    { $project: { flow: { id: '$flow._id', title: 1 }, count: 1 } },
+  ];
+}
 
+/**
+ * Get the most used flows.
+ * A flow to be used needs to start, so it is based on activities that start the flow
+ */
 function getMostUsedFlows() {
+  const mostUsedFlowsAggregationPipeline = createFlowAggregationPipeline('start');
   return Activity.aggregate(mostUsedFlowsAggregationPipeline);
 }
 
-module.exports = { getMostUsedFlows };
+/**
+ * Get the most completed flows.
+ * A flow to be completed needs to end, so it is based on activities that end the flow
+ */
+function getMostCompletedFlows() {
+  const mostCompletedFlowsAggregationPipeline = createFlowAggregationPipeline('end');
+  return Activity.aggregate(mostCompletedFlowsAggregationPipeline);
+}
+
+module.exports = { getMostCompletedFlows, getMostUsedFlows };
